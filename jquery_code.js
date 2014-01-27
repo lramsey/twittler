728b2b7fa344a307e4970cdebd14252eba5232f5
lramsey <lramsey177@gmail.com>
2014-01-26 17:05:41 -0800

    cleaned up sloppy code layout

--- jquery_code.js
+++ jquery_code.js
@@ -1,47 +1,47 @@
-      $(document).ready(function(){
+$(document).ready(function(){
         
-        var $span = $('span');
-        $span.html('');
+  var $span = $('span');
+  $span.html('');
         
-        var posting = function(input){
-          var tweet = input;
-          var $tweet = $('<div></div>');
-          var text = '<b>@' +tweet.user +'</b>' + ': ' + tweet.message + ".\n created on " + tweet.created_at;
-          var $text = $tweet.append(text);
-          $tweet.appendTo($span);
-        };
+  var posting = function(input){
+    var tweet = input;
+    var $tweet = $('<div></div>');
+    var text = '<b>@' +tweet.user +'</b>' + ': ' + tweet.message + ". created on " + tweet.created_at;
+    var $text = $tweet.append(text);
+    $tweet.appendTo($span);
+  };
 
-        var profileClick = function() {
-          $('span').find('b').on('click', function(){
-            $('span').children().remove();
-            var name = $(this).text().slice(1);
-            var profile = streams.users[name];
-            var profileIndex = profile.length -1;
-            while(profileIndex >= 0){
-              posting(profile[profileIndex]);
-              profileIndex -= 1;
-            }
-            profileClick();
-          });
-        };
+  var profileClick = function() {
+    $('span').find('b').on('click', function(){
+      $('span').children().remove();
+      var name = $(this).text().slice(1);
+      var profile = streams.users[name];
+      var profileIndex = profile.length -1;
+      while(profileIndex >= 0){
+        posting(profile[profileIndex]);
+        profileIndex -= 1;
+      }
+      profileClick();
+    });
+  };
 
-        var index = streams.home.length - 1;
-        while (index >= 0){
-          posting(streams.home[index]);
-          index -= 1;
-        }
+  var index = streams.home.length - 1;
+  while (index >= 0){
+    posting(streams.home[index]);
+    index -= 1;
+  }
 
-        profileClick();
+  profileClick();
 
-        $('button').on('click', function(){
-          $span.html('');
-          var index = streams.home.length - 1;
-          while(index >= 0){
-            posting(streams.home[index]);
-            $('span').children().first().next().next().next().next().next().next().next().next().next().next().next().next().next().next().next().next().next().next().next().next().remove();
-            index -= 1;
-          }
-          profileClick();
-        });
-
-      });
+  $('button').on('click', function(){
+    $span.html('');
+    var index = streams.home.length - 1;
+    while(index >= 0){
+      posting(streams.home[index]);
+      $('span').children().first().next().next().next().next().next().next().next().next().next().next().next().next().next().next().next().next().next().next().next().next().remove();
+      index -= 1;
+    }
+    profileClick();
+  });
+  
+});
