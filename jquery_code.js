fad81dca235fad54b4f407f67dc970d41b5bde7d
lramsey <lramsey177@gmail.com>
2014-01-30 00:42:41 -0800

    change button to 10 most recent per page and new css

--- jquery_code.js
+++ jquery_code.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
   var $span = $('span');
   $span.html('');
         
-  var posting = function(input){
-    var tweet = input;
+  var posting = function(item){
+    var tweet = item;
     var $tweet = $('<div></div>');
     var text = '<b>@' +tweet.user +'</b>' + ': ' + tweet.message + ". created on " + tweet.created_at;
     var $text = $tweet.append(text);
@@ -13,13 +13,16 @@ $(document).ready(function(){
 
   var headerClick = function() {
     $('h1').on('click', function() {
+      $('span').children().remove();
       var index = streams.home.length - 1;
       while (index >= 0){
         posting(streams.home[index]);
         index -= 1;
       }
-      headerClick();
+      $('input').val("10 Newest Tweets");
+      $('input').removeClass('profile');
       profileClick();
+      button(streams.home);
     });
   };
 
@@ -33,9 +36,25 @@ $(document).ready(function(){
         posting(profile[profileIndex]);
         profileIndex -= 1;
       }
-      headerClick();
+      $('input').val("@" + name + "'s 10 Newest Tweets");
+      $('input').addClass('profile');
       profileClick();
+      button(profile);
     });
+  };
+
+  var button = function(page){
+    $('input').on('click', function(){
+      $span.html('');
+      var index = page.length - 1;
+      while(index >= 0){
+        posting(page[index]);
+        $('span').children().first().next().next().next().next().next().next().next().next().next().next().remove();
+        index -= 1;
+      }
+      profileClick();
+  });
+
   };
 
   var index = streams.home.length - 1;
@@ -46,17 +65,6 @@ $(document).ready(function(){
 
   headerClick();
   profileClick();
-
-  $('input').on('click', function(){
-    $span.html('');
-    var index = streams.home.length - 1;
-    while(index >= 0){
-      posting(streams.home[index]);
-      $('span').children().first().next().next().next().next().next().next().next().next().next().next().next().next().next().next().next().next().next().next().next().next().remove();
-      index -= 1;
-    }
-    headerClick();
-    profileClick();
-  });
-
+  button(streams.home);
+  
 });
